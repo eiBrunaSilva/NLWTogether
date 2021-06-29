e92b5eda938f6ebbeaa96b8cf6bbd298233ad6f0
Bruna Nunes da Silva <silvabruna.email@gmail.com>
2021-06-28 22:47:26 -0300

    letmeask

---
+++ letmeask/src/components/Button.tsx
@@ -0,0 +1,11 @@
+import { ButtonHTMLAttributes } from "react";
+import "../styles/button.scss";
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  isOutline?: boolean;
+};
+export function Button({ isOutline = false, ...props }: ButtonProps) {
+  return (
+    <button className={`button${isOutline ? "outline" : ""}`} {...props} />
+  );
+}
---
+++ letmeask/src/pages/AdminRoom.tsx
@@ -0,0 +1,111 @@
+import logoImg from "../assets/images/logo.svg";
+import { Button } from "../components/Button";
+import { RoomCode } from "../components/RoomCode";
+import "../styles/room.scss";
+import { useHistory, useParams } from "react-router-dom";
+import { useRoom } from "../hooks/useRoom";
+import { Question } from "../components/Question";
+import deleteImg from "../assets/images/delete.svg";
+import { database } from "../services/firebase";
+import checkImg from "../assets/images/check.svg";
+import answerImg from "../assets/images/answer.svg";
+
+type RoomParams = {
+  id: string;
+};
+
+export function AdminRoom() {
+  const history = useHistory();
+  const params = useParams<RoomParams>();
+  const roomId = params.id;
+  const { title, question } = useRoom(roomId);
+
+  async function handleEndRoom() {
+    await database.ref(`rooms/${roomId}`).update({
+      endedAt: new Date(),
+    });
+
+    history.push("/");
+  }
+
+  async function handleDeleteQuestion(questionId: string) {
+    if (window.confirm("Tem certeza que deseja excluir esta pergunta?")) {
+      await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+    }
+  }
+
+  async function handleCheckQuestionAsAnswered(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isAnswered: true,
+    });
+  }
+
+  async function handleHighLightQuestion(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isHighLighted: true,
+    });
+  }
+
+  return (
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="Letmeask" />
+          <div>
+            <RoomCode code={roomId} />
+            <Button isOutline onClick={handleEndRoom}>
+              Encerrar sala
+            </Button>
+          </div>
+        </div>
+      </header>
+
+      <main className="content">
+        <div className="room-title">
+          <h1>Sala {title}</h1>
+          {question.length > 0 && <span>{question.length} pergunta(s)</span>}
+        </div>
+
+        <div className="question-list">
+          {question.map((question) => {
+            return (
+              <Question
+                key={question.id}
+                content={question.content}
+                author={question.author}
+              >
+                {!question.isAnswered && (
+                  <>
+                    {" "}
+                    <button
+                      type="button"
+                      onClick={() => handleCheckQuestionAsAnswered(question.id)}
+                    >
+                      <img
+                        src={checkImg}
+                        alt="Marcar pergunta como respondida"
+                      />
+                    </button>
+                    <button
+                      type="button"
+                      onClick={() => handleHighLightQuestion(question.id)}
+                    >
+                      <img src={answerImg} alt="Dar destaque á pergunta" />
+                    </button>
+                  </>
+                )}
+
+                <button
+                  type="button"
+                  onClick={() => handleDeleteQuestion(question.id)}
+                >
+                  <img src={deleteImg} alt="remover pergunta" />
+                </button>
+              </Question>
+            );
+          })}
+        </div>
+      </main>
+    </div>
+  );
+}
